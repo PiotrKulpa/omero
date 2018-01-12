90441e40f5bcff0a8de2554219ea4968539b7796
Piotr Kulpa <p.kulpa@onet.eu>
2018-01-12 16:21:00 +0100

    slider init

--- src/assets/js/banner.js
+++ src/assets/js/banner.js
@@ -1,2 +1,19 @@
-var a = 0;
-var b = 'jeden';
+var i = 0, x = 0, slide = 0;
+$('.arrow-wrapper').on('click','.left-arrow', function () {
+  slide+=-33.3;
+  $('#figure').css({
+    'transform': 'translate('+ slide +'%, 0)',
+    'transition': 'transform 1s ease'
+  });
+  console.log(slide);
+});
+
+$('.arrow-wrapper').on('click','.right-arrow', function () {
+  slide+=33.3;
+  $('#figure').css({
+    'transform': 'translate('+ slide +'%, 0)',
+    'transition': 'transform 1s ease'
+  });
+  console.log(slide);
+
+});
--- src/assets/js/banner.ts
+++ src/assets/js/banner.ts
@@ -1,2 +1,18 @@
-let a: number = 0;
-let b: string = 'jeden';
+var i = 0, x = 0, slide = 0;
+$('.left').click(function () {
+  slide+=-33.3;
+  $('#figure').css({
+    'transform': 'translate('+ slide +'%, 0)',
+    'transition': 'transform 1s ease'
+  });
+  console.log(slide);
+});
+
+$('.right').click(function () {
+  slide+=33.3;
+  $('#figure').css({
+    'transform': 'translate('+ slide +'%, 0)',
+    'transition': 'transform 1s ease'
+  });
+  console.log(slide);
+});
